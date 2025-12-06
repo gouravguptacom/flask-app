e8d7bbb960ae14eb73bfb8c4d705a67b3cb6dca0
Gourav Gupta <sorenpinky96@gmail.com>
2025-12-06 03:12:14 +0000

    added jinga templates

--- app.py
+++ app.py
@@ -1,10 +1,35 @@
-from flask import Flask
+from flask import Flask, render_template, redirect, url_for
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder="templates")
 
 @app.route('/')
 def index():
-    return "Hello World"
+    myvalue = "Raisehand"
+    myresult = 10 + 20
+    mylist = [10, 20, 30, 40, 50]
+    return render_template("index.html", myvalue=myvalue, anyname=myresult, mylist=mylist)
+
+@app.route('/other')
+def other():
+    some_text = "Hello World"
+    return render_template("other.html", some_text=some_text)
+
+@app.route('/redirect_endpoint')
+def redirect_endpoint():
+    return redirect(url_for('other'))
+
+
+@app.template_filter("reverse_string")
+def reverse_string(s):
+    return s[::-1]
+
+@app.template_filter("repeat")
+def repeat(s, times = 2):
+    return s * times
+
+@app.template_filter("alternate_case")
+def alternate_case(s):
+    return ''.join([c.upper() if i % 2 == 0 else c.lower() for i, c in enumerate(s)])
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=5555, debug=True)

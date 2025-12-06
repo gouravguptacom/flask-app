d3ca4c58b276b0a9b3e2b786900b9f21dc3e6177
Gourav Gupta <sorenpinky96@gmail.com>
2025-12-06 07:09:10 +0000

    Session & Cookies with flash

--- app.py
+++ app.py
@@ -1,10 +1,63 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, session, make_response, request, flash
 
 app = Flask(__name__, template_folder="templates")
+app.secret_key = "SOME KEY"
 
 @app.route("/")
 def index():
-    return render_template("index.html")
+    return render_template("index.html", message="Index")
+
+# client can change, it's unreadable [secure then cookie]
+@app.route("/set_data")
+def set_data():
+    session['name'] = 'Gaurav'
+    session['other'] = 'Hello, World'
+
+    return render_template("index.html", message="Session data set.")
+
+@app.route("/get_data")
+def get_data():
+    name = session.get('name')
+    other = session.get('other')
+    return render_template("index.html", message=f"Name: {name}, Other: {other}")
+
+@app.route("/clear_session")
+def clear_session():
+    session.clear()
+    # session.pop("name")
+    return render_template("index.html", message=f"Session cleared.")
+
+# client can change too, it's visible
+@app.route("/set_cookie")
+def set_cookie():
+    response = make_response(render_template("index.html", message="Cookie set."))
+    response.set_cookie("cookie_name", "cookie_value")
+    return response
+
+@app.route("/get_cookie")
+def get_cookie():
+    cookie_value = request.cookies.get("cookie_name")
+    return render_template("index.html", message=f"Cookie value: {cookie_value}")
+
+@app.route("/remove_cookie")
+def remove_cookie():
+    response = make_response(render_template("index.html", message="Cookie removed."))
+    response.set_cookie("cookie_name", expires=0)
+    return response
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    if request.method == 'GET':
+        return render_template("login.html")
+    elif request.method == 'POST':
+        username = request.form.get('username')
+        password = request.form.get('password')
+        if username == 'raisehand' and password == 'password':
+            flash("Successful Login!")
+            return render_template("index.html", message="")
+        else:
+            flash("Login failed!")
+            return render_template("index.html", message="")
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=5555, debug=True)
